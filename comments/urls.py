e62ab3f5da6321a71fa185acfcf4eb99c2c966ff
Shimtu <sshalmiev@yandex.ru>
2025-03-29 15:13:51 +0300

    Первый коммит проекта InfoHub

--- comments/urls.py
+++ comments/urls.py
@@ -14,4 +14,5 @@ from .views import add_comment, delete_comment
 urlpatterns = [
     path("game/<int:game_id>/comment/", add_comment, name="add_comment"),
     path("delete/<int:comment_id>/", delete_comment, name="delete_comment"),
+    path("logout/", auth_views.LogoutView.as_view(template_name="account/logout.html"), name="logout")
 ]
